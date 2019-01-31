4aef88d776427701a7af1b7886349b0a982c86bb
Misha <mkaravaev@gmail.com>
2019-02-01 00:30:28 +0500

    add: integer to list parser error for wrong integers

--- apps/checksum_calculator/lib/checksum_calculator/digits_parser.ex
+++ apps/checksum_calculator/lib/checksum_calculator/digits_parser.ex
@@ -3,12 +3,13 @@ defmodule ChecksumCalculator.DigitsParser do
   parse multi digit integer to list of digits
   """
 
-  @spec int_to_digits_list(integer) :: list(integer)
-  def int_to_digits_list(int) do
+  @spec int_to_digits_list(non_neg_integer) :: list(non_neg_integer)
+  def int_to_digits_list(int) when is_integer(int) and int >= 0 do
     int
     |> from_int_to_charlist()
     |> from_charlist_to_int_list
   end
+  def int_to_digits_list(_), do: {:error, :wrong_integer}
 
   defp from_charlist_to_int_list(charlist) when is_list(charlist) do
     Enum.map(charlist, &from_ascii_to_int/1)
--- apps/checksum_calculator/test/checksum_calculator/digits_parser_test.exs
+++ apps/checksum_calculator/test/checksum_calculator/digits_parser_test.exs
@@ -6,7 +6,12 @@ defmodule ChecksumCalculator.DigitsParserTest do
     test "should convert integer to list of digits" do
       assert DigitsParser.int_to_digits_list(1234) == [1,2,3,4]
       assert DigitsParser.int_to_digits_list(10) == [1,0]
-      assert DigitsParser.int_to_digits_list(1) == [1]
+      assert DigitsParser.int_to_digits_list(0) == [0]
+    end
+
+    test "should return error if integer is less than 0" do
+      assert DigitsParser.int_to_digits_list(-1) == {:error, :wrong_integer}
+      assert DigitsParser.int_to_digits_list(-1222) == {:error, :wrong_integer}
     end
   end
 end
